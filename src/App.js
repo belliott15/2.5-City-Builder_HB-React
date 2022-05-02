cabde48ba3417b0865bf92adc82dcd6fcdc66fb1
Beau Elliott <belliott1516@gmail.com>
2022-05-02 11:54:17 -0700

    commented line 2 back in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { useState } from 'react';
-// import './App.css';
+import './App.css'; 
 // import SkylineDropdown from './SkylineDropdown';
 // import WaterfrontDropdown from './WaterfrontDropdown';
 // import CastleDropdown from './CastleDropdown';
