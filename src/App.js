3f23890bc0f3098ab66438d0b69193036022b756
Beau Elliott <belliott1516@gmail.com>
2022-05-02 12:17:37 -0700

    initialized photos and city name input

--- src/App.js
+++ src/App.js
@@ -11,18 +11,25 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineID, setSkylineID] = useState(1);
+  const [waterFrontID, setWaterFrontID] = useState(1);
+  const [castleID, setCastleID] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [slogans, setSlogans] = useState(['']);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City skylineID={skylineID} waterFrontID={waterFrontID} castleID={castleID} />
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput setCityName={setCityName}/>
         <section className='dropdowns'>
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
--- src/City.js
+++ src/City.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-export default function City(props) {
+export default function City({ skylineID, waterFrontID, castleID }) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      <img src={ `skyline-${skylineID}.jpeg`} />
+      <img src={ `waterfront-${waterFrontID}.jpeg`} />
+      <img src={ `castle-${castleID}.jpeg`} />
     </div>
   );
 }
